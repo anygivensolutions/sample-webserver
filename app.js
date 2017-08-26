27fc2064c1c116f8689dfb647d14bdf7f42d5fec
Dom <anygivensolutions@users.noreply.github.com>
2017-08-26 06:21:25 -0700

    added new project page

--- app.js
+++ app.js
@@ -53,6 +53,13 @@ app.get('/bad', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects', {
+    pageTitle: 'Projects',
+    welcomeMessage: 'View my projects'
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
